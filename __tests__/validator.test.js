0d845fd384b81e8345fab3738fb44b6b1c986693
Nadya Ilinskaya <nadya.ilinskiy@gmail.com>
2019-09-30 16:40:24 -0700

    update validator and tests to validate simple cases / make first test suite to pass

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -6,13 +6,15 @@ describe('validator module performs basic validation of', () => {
 
   // TODO: Make this series of tests less repetitive ... DRY it out
 
+  let str = 'yes';
+  let num = 1;
+  let arr = ['a'];
+  let obj = {x:'y'};
+  let func = () => {};
+  let bool = false;
+
+
   it('strings', () => {
-    let str = 'yes';
-    let num = 1;
-    let arr = ['a'];
-    let obj = {x:'y'};
-    let func = () => {};
-    let bool = false;
     expect(validator.isString(str)).toBeTruthy();
     expect(validator.isString(num)).toBeFalsy();
     expect(validator.isString(arr)).toBeFalsy();
@@ -22,23 +24,48 @@ describe('validator module performs basic validation of', () => {
   });
 
   it('numbers', () => {
-    expect(true).toBeFalsy();
+    expect(validator.isNumber(num)).toBeTruthy();
+    expect(validator.isNumber(str)).toBeFalsy();
+    expect(validator.isNumber(arr)).toBeFalsy();
+    expect(validator.isNumber(obj)).toBeFalsy();
+    expect(validator.isNumber(func)).toBeFalsy();
+    expect(validator.isNumber(bool)).toBeFalsy();
   });
 
   it('arrays', () => {
-    expect(true).toBeFalsy();
+    expect(validator.isArray(arr)).toBeTruthy();
+    expect(validator.isArray(num)).toBeFalsy();
+    expect(validator.isArray(str)).toBeFalsy();
+    expect(validator.isArray(obj)).toBeFalsy();
+    expect(validator.isArray(func)).toBeFalsy();
+    expect(validator.isArray(bool)).toBeFalsy();
   });
 
   it('objects', () => {
-    expect(true).toBeFalsy();
+    expect(validator.isObject(obj)).toBeTruthy();  
+    expect(validator.isObject(arr)).toBeFalsy();
+    expect(validator.isObject(num)).toBeFalsy();
+    expect(validator.isObject(str)).toBeFalsy();
+    expect(validator.isObject(func)).toBeFalsy();
+    expect(validator.isObject(bool)).toBeFalsy();
   });
 
   it('booleans', () => {
-    expect(true).toBeFalsy();
+    expect(validator.isBoolean(bool)).toBeTruthy();
+    expect(validator.isBoolean(obj)).toBeFalsy(); 
+    expect(validator.isBoolean(arr)).toBeFalsy();
+    expect(validator.isBoolean(num)).toBeFalsy();
+    expect(validator.isBoolean(str)).toBeFalsy();
+    expect(validator.isBoolean(func)).toBeFalsy();
   });
 
   it('functions', () => {
-    expect(true).toBeFalsy();
+    expect(validator.isFunction(func)).toBeTruthy();
+    expect(validator.isFunction(obj)).toBeFalsy(); 
+    expect(validator.isFunction(arr)).toBeFalsy();
+    expect(validator.isFunction(num)).toBeFalsy();
+    expect(validator.isFunction(str)).toBeFalsy(); 
+    expect(validator.sFunction(bool)).toBeFalsy();
   });
 
 });
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 'use strict';
 
 const validator = require('./lib/validator.js');
+
+let str = 'однажды в студеную зимнюю пору';
+//let str = [1, 2, 3];
+console.log ('string validation => ' + str, validator.isValid(str, 'string') );
